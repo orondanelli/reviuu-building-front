197421fcbd364a0ce29081e514bf6434cb74833b
Sebastian Hondarza <sebastian.hondarza@gmail.com>
2022-04-26 20:55:33 -0400

    🎨  height & width sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import Sidenav from "./components/Sidenav.vue";
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
-        <router-view></router-view>
+      <router-view></router-view>
     </el-container>
   </div>
 </template>
